36f8e09c3f97579553f899d841b8546b2de6bb4f
Huong Huong <huongnguyen08.cv@gmail.com>
2022-05-26 21:53:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://user12345678:iUgdVb8fbnX4CTZo@cluster0.gxiry.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected'))
 
-app.get('/', (req, res) => {
-  res.send({
-    name: 'BEN4',
-    key: 'abc'
-  });
+const StudentModel = require('./StudentModel');
+
+app.get('/', async (req, res) => {
+   await StudentModel.insertMany([
+     { name: 'Nguyen Van A', email: 'a@gmail.com', tags: [{ id: 1, value: "AAAAA" }, { id: 2, value: "BBBBB" }, { id: 3, value: "AAAAA" }, { id: 4, value: "BBBBB" }], class: '10A1' },
+     { name: 'Nguyen Van B', email: 'b@gmail.com', tags: [{ id: 1, value: "AAAAA" }, { id: 2, value: "BBBBB" }, { id: 3, value: "AAAAA" }, { id: 4, value: "BBBBB" }], class: '10A1' }
+    ]);
+   const studentA = await StudentModel.find({
+      email: 'a@gmail.com'
+    });
+   return res.send({
+      name: 'BEN4',
+      key: 'abc',
+      student: studentA
+    });
 });
 app.listen(process.env.PORT, (req, res) => {
   console.log('Server listening on port '+process.env.PORT);
